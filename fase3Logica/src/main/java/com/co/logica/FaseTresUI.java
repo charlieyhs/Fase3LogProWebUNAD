f0b86528d04ede4dd0089374bc702564ca7ed7e2
Charlieyhs <87665482+charlieyhs@users.noreply.github.com>
2023-11-11 21:51:22 -0500

    Update FaseTresUI.java
    ajuste para arrojar excepciones

--- fase3Logica/src/main/java/com/co/logica/FaseTresUI.java
+++ fase3Logica/src/main/java/com/co/logica/FaseTresUI.java
@@ -3,7 +3,9 @@ package com.co.logica;
 import com.co.DTO.ExportacionDTO;
 import com.co.DTO.PersonaDTO;
 import com.co.DTO.ProductoDTO;
+import com.co.DTO.TasaConversionDTO;
 import com.co.servicios.ExportarSvc;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -40,44 +42,66 @@ public class FaseTresUI {
         if(productosExportar == null){
             productosExportar = new ArrayList<>();
         }
-        if(producto.getNombre() == null || producto.getNombre().trim().equals("")){
+        try{
+            if(producto.getNombre() == null || producto.getNombre().trim().equals("")){
+                throw new Exception("Debe seleccionar un producto");
+            }
+            productosExportar.add(producto);
+            producto = new ProductoDTO();
+            severity = "info";
+            mensaje = "Producto agregado correctamente";
+        }catch(Exception e){
             severity = "warn";
-            mensaje = "Debe seleccionar un producto";
-            return;
+            mensaje = e.getMessage();
         }
-        productosExportar.add(producto);
-        producto = new ProductoDTO();
-        severity = "info";
-        mensaje = "Producto agregado correctamente";
     }
     public void exportar(){
-        PersonaDTO remitente = new PersonaDTO();
-        remitente.setCargo("REMITENTE");
-        remitente.setNombre("REMITENTE");
+        try{
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            PersonaDTO remitente = new PersonaDTO();
+            remitente.setCargo("REMITENTE");
+            remitente.setNombre("REMITENTE");
+            remitente.setFechaNacimiento(sdf.parse("1990-01-01"));
+            int edadRemitente = remitente.calcularEdad();
+            
+            if(edadRemitente < 18){
+                throw new Exception("El remitente no puede exportar porque es menor de edad.");
+            }
+            
+            PersonaDTO destinatario = new PersonaDTO();
+            destinatario.setCargo("DESTINATARIO");
+            destinatario.setNombre("DESTINATARIO");
+            destinatario.setFechaNacimiento(sdf.parse("1980-01-01"));
+            int edadDestinatario = destinatario.calcularEdad();
+            
+            if(edadDestinatario < 18){
+                throw new Exception("El destinatario no puede exportar porque es menor de edad.");
+            }
+            
+            ExportacionDTO exportacion = new ExportacionDTO();
+            exportacion.setConsecutivo(String.valueOf(System.currentTimeMillis()));
+            exportacion.setProductos(productosExportar);
+            exportacion.setFormaEnvio(ExportacionDTO.formasEnvio.get(0));
+            exportacion.setFechaEnvio(new Date());
+            exportacion.setFechaEstimadaLlegada(new Date());
+            exportacion.setRemitente(remitente);
+            exportacion.setDestinatario(destinatario);
+            exportacion.setFormaPago(ExportacionDTO.formasPago.get(0));
+            exportacion.setValorImpuestos(15000);
+            exportacion.setMonedaPago("COP");
+            exportacion.setValorTotal(150000.0);
+
+            ExportarSvc exporSvc = new ExportarSvc();
+            exporSvc.exportar(exportacion);
+            productosExportar = new ArrayList<>();
+            producto = new ProductoDTO();
+            severity = "info";
+            mensaje = "Exportación realizada correctamente";
+        }catch(Exception e){
+            severity = "warn";
+            mensaje = e.getMessage();
+        }
         
-        PersonaDTO destinatario = new PersonaDTO();
-        destinatario.setCargo("DESTINATARIO");
-        destinatario.setNombre("DESTINATARIO");
-        
-        ExportacionDTO exportacion = new ExportacionDTO();
-        exportacion.setConsecutivo(String.valueOf(System.currentTimeMillis()));
-        exportacion.setProductos(productosExportar);
-        exportacion.setFormaEnvio(ExportacionDTO.formasEnvio.get(0));
-        exportacion.setFechaEnvio(new Date());
-        exportacion.setFechaEstimadaLlegada(new Date());
-        exportacion.setRemitente(remitente);
-        exportacion.setDestinatario(destinatario);
-        exportacion.setFormaPago(ExportacionDTO.formasPago.get(0));
-        exportacion.setValorImpuestos(15000);
-        exportacion.setMonedaPago("COP");
-        exportacion.setValorTotal(150000.0);
-        
-        ExportarSvc exporSvc = new ExportarSvc();
-        exporSvc.exportar(exportacion);
-        productosExportar = new ArrayList<>();
-        producto = new ProductoDTO();
-        severity = "info";
-        mensaje = "Exportación realizada correctamente";
     }
 
     public List<ProductoDTO> getProductosExportar() {
